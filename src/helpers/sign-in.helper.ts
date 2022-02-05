29e317d689e87b03f44c8b530ac82b4720fab4c4
Petro Franko <petro.franko@demodesk.com>
2022-02-05 13:02:21 +0100

    created sign-in helper

---
+++ src/helpers/sign-in.helper.ts
@@ -0,0 +1,29 @@
+import { Page } from '@playwright/test';
+import { MainPage } from '../pages/main.page';
+import { SignInDialog } from '../pages/sign-in.dialog';
+import { TempEmail } from '../pages/temp-email.page';
+
+export const signIn = async (page: Page) => {
+  const context = page.context();
+  const emailPage = await context.newPage();
+  const tempEmailPage = new TempEmail(emailPage);
+  await tempEmailPage.goto();
+  const email = await tempEmailPage.getTempEmail();
+
+  const mainPage = new MainPage(page);
+  await mainPage.goto();
+  await mainPage.signInBtn.click();
+
+  const signInDialog = new SignInDialog(page);
+  await signInDialog.emailInput.fill(email);
+  await signInDialog.loginWithEmailBtn.click();
+
+  await tempEmailPage.emailTitle.waitFor();
+  await tempEmailPage.emailTitle.click();
+  const signInLinkEl = tempEmailPage.emailFrame.locator('a:text("Sign in")');
+  const link = await signInLinkEl.getAttribute('href');
+
+  await emailPage.close();
+  await page.goto(link);
+  return email;
+};
