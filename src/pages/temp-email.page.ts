fd21cc1eba90040bf0d147a8d8a2675434332dac
Petro Franko <petro.franko@demodesk.com>
2022-02-07 12:24:46 +0100

    removed comments

--- src/pages/temp-email.page.ts
+++ src/pages/temp-email.page.ts
@@ -2,14 +2,10 @@ import { Page } from '@playwright/test';
 
 export class TempEmail {
   constructor(public readonly page: Page) {}
-  // emailInput = this.page.locator('//input[@id="i-email"]');
   emailInput = this.page.locator('[id="temp-mail"]');
-  // emailTitle = this.page.locator('[class="title"]:text("qa-challenge@pego.dev")');
   emailTitle = this.page.locator('[class="sendename"]:has-text("qa-challenge@pego.dev")');
-  // emailFrame = this.page.frameLocator('[id="fullmessage"]');
 
   async goto() {
-    // await this.page.goto('https://tempmail.dev/');
     await this.page.goto('https://tempmail.tel/');
     await this.page.waitForTimeout(2000);
   }
