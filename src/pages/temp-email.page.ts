ccd9833c79b0b236326b8fcfc9284e35e813c752
Petro Franko <petro.franko@demodesk.com>
2022-02-07 12:22:01 +0100

    finished temp email page

--- src/pages/temp-email.page.ts
+++ src/pages/temp-email.page.ts
@@ -2,13 +2,16 @@ import { Page } from '@playwright/test';
 
 export class TempEmail {
   constructor(public readonly page: Page) {}
-  emailInput = this.page.locator('//input[@id="i-email"]');
-  emailTitle = this.page.locator('[class="title"]:text("qa-challenge@pego.dev")');
-  emailFrame = this.page.frameLocator('[id="fullmessage"]');
+  // emailInput = this.page.locator('//input[@id="i-email"]');
+  emailInput = this.page.locator('[id="temp-mail"]');
+  // emailTitle = this.page.locator('[class="title"]:text("qa-challenge@pego.dev")');
+  emailTitle = this.page.locator('[class="sendename"]:has-text("qa-challenge@pego.dev")');
+  // emailFrame = this.page.frameLocator('[id="fullmessage"]');
 
   async goto() {
-    await this.page.goto('https://tempmailo.com/', { waitUntil: 'domcontentloaded' });
-    await this.emailInput.waitFor();
+    // await this.page.goto('https://tempmail.dev/');
+    await this.page.goto('https://tempmail.tel/');
+    await this.page.waitForTimeout(2000);
   }
 
   async getTempEmail() {
