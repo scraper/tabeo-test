202836e2a396f37201c96064248ac8cf0ac76398
Petro Franko <petro.franko@demodesk.com>
2022-02-07 12:19:57 +0100

    finished test for buying icons

---
+++ src/tests/buy.test.ts
@@ -0,0 +1,54 @@
+import test, { expect } from '@playwright/test';
+import { signIn } from '../helpers/sign-in.helper';
+import { MainPage } from '../pages/main.page';
+import { CheckoutPage } from '../pages/stripe/checkout.page';
+import { ThankYouPage } from '../pages/thank-you.page';
+
+test.describe('Buy icons', async () => {
+  test('Pay monthly with valid payment details', async ({ page }) => {
+    await signIn(page);
+
+    const mainPage = new MainPage(page);
+    await mainPage.payMonthlyBtn.click();
+
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.waitToLoad();
+    await checkoutPage.enterCardDetails('4000002760003184');
+
+    const thankYouPage = new ThankYouPage(page);
+    await expect(thankYouPage.successMsg).toBeVisible();
+    await expect(thankYouPage.license).toBeVisible();
+    await expect(thankYouPage.monthlyPriceDetails).toBeVisible();
+    await expect(thankYouPage.downloadBtn).toBeVisible();
+  });
+  test('Pay yearly with valid payment details', async ({ page }) => {
+    await signIn(page);
+
+    const mainPage = new MainPage(page);
+    await mainPage.payOnce.click();
+
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.waitToLoad();
+    await checkoutPage.enterCardDetails('4000002760003184');
+
+    const thankYouPage = new ThankYouPage(page);
+    await expect(thankYouPage.successMsg).toBeVisible();
+    await expect(thankYouPage.license).toBeVisible();
+    await expect(thankYouPage.singlePriceDetails).toBeVisible();
+    await expect(thankYouPage.downloadBtn).toBeVisible();
+  });
+
+  test('Pay yearly with invalid payment details', async ({ page }) => {
+    await signIn(page);
+
+    const mainPage = new MainPage(page);
+    await mainPage.payOnce.click();
+
+    const checkoutPage = new CheckoutPage(page);
+    await checkoutPage.waitToLoad();
+    await checkoutPage.enterCardDetails('4000008260003178');
+    await page.pause();
+    await checkoutPage.cardDeclinedErrMsg.waitFor();
+    await expect(checkoutPage.cardDeclinedErrMsg).toBeVisible();
+  });
+});
