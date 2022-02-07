55521399b1554c974fcf8c1cc656b11630660d55
Petro Franko <petro.franko@demodesk.com>
2022-02-07 13:17:09 +0100

    implemented waitToLoad() method

--- src/pages/thank-you.page.ts
+++ src/pages/thank-you.page.ts
@@ -3,6 +3,10 @@ import { Page } from '@playwright/test';
 export class ThankYouPage {
   constructor(public readonly page: Page) {}
 
+  async waitToLoad() {
+    await this.page.waitForNavigation();
+    await this.successMsg.waitFor({ timeout: 30000 });
+  }
   successMsg = this.page.locator('text=Your purchase is ready to be downloaded.');
   license = this.page.locator('dt:has-text("License")');
   monthlyPriceDetails = this.page.locator('dd:has-text("12 * £20 (£240)")');
